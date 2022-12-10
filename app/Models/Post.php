d3c3b7bc475d6e44dec5fac119c5a102d1410b96
“otunola” <otunolaaws@gmail.com>
2022-12-10 03:02:40 +0000

    commit -m

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,9 @@ class Post extends Model
     {
         return $this->belongsTo('User');
     }
+    //post has many comments
+    public function comments()
+    {
+        return $this->hasMany('Comment');
+    }
 }
